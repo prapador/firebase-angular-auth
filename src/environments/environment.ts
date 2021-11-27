af0b3f988ffdc5cb0521b56a8d7bba4a86fefe17
Sergio Martínez <sergiotr94@gmail.com>
2021-11-27 20:10:03 +0100

    npm firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'intalentouth',
+    appId: '1:479044123677:web:24f7c9ed4656738a279af4',
+    storageBucket: 'intalentouth.appspot.com',
+    apiKey: 'AIzaSyC7TRhyPyP3TVVg-oCQkTjqdUlD43PmhZA',
+    authDomain: 'intalentouth.firebaseapp.com',
+    messagingSenderId: '479044123677',
+  },
   production: false
 };
 
